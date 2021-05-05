dec8d402f76fd8ccdd21098701241814d0ddf80d
Achref Bouthouri <Achref.Bouthouri11@gmail.com>
2021-05-05 11:17:24 +0100

    add upload

--- src/app/Pages/create-place/create-place.page.ts
+++ src/app/Pages/create-place/create-place.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ModalController, NavParams, LoadingController } from '@ionic/angular';
+import { ModalController, NavParams, LoadingController, ActionSheetController } from '@ionic/angular';
 import { PlaceService } from 'src/app/services/place.service';
-import { Storage } from '@ionic/storage-angular'
+import { Storage } from '@ionic/storage-angular';
+import { Camera,CameraOptions } from '@ionic-native/camera/ngx'
 import { Place } from 'src/app/models/Place';
 import { latLng } from 'leaflet';
 const ID_USER = 'id';
@@ -15,12 +16,16 @@ export class CreatePlacePage implements OnInit {
   id: number;
   toggleValue: boolean = true;
   credentialsForm: FormGroup;
+  based64Image: String;
   constructor(
     private modalController: ModalController,
     private navParams: NavParams,
     private formBuilder: FormBuilder,
     private placeService: PlaceService,
-    private storage: Storage
+    private storage: Storage,
+    public loadingController: LoadingController,
+    private actionSheetCtrl: ActionSheetController,
+    private camera: Camera
   ) { }
 
   ngOnInit() {
@@ -64,6 +69,57 @@ export class CreatePlacePage implements OnInit {
     console.log('___', this.credentialsForm.value)
   }
 
+  openCamera(){
+    const option: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(option).then((imageData) => {
+      this.based64Image = 'data:image/jpeg;based64,' +imageData;
+    })
+  }
+
+  openGallery(){
+    const option: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+      
+    }
+    this.camera.getPicture(option).then((imageData) => {
+      this.based64Image = 'data:image/jpeg;based64,' +imageData;
+    })
+  }
+
+  async presentActionSheet() {
+    let actionSheet = await this.actionSheetCtrl.create({
+      //title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.openGallery();
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.openCamera();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
   Submit() {
     // if (this.credentialsForm.invalid) {
     //   console.log('invalid form');
@@ -86,6 +142,21 @@ export class CreatePlacePage implements OnInit {
         });
       }, 2000);
     //}
+  }
+
+
+
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Please wait...',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
   }
 
   // Submit() {
